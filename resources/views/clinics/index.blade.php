ecc1150ee3fdf0ef6ed8e51fd4c404d7c796eae3
Diego F. Ferreira <diegosk8zero@gmail.com>
2024-11-07 12:58:32 +0100

    adicionado data tables

--- resources/views/clinics/index.blade.php
+++ resources/views/clinics/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
 <?php foreach($clinics as $clinic) :?>
-<div class="container mx-auto px-10">
+<!-- <div class="container mx-auto px-10">
 <ul role="list" class="divide-y divide-gray-100">
   <li class="flex justify-between gap-x-6 py-5">
     <div class="flex min-w-0 gap-x-4">
@@ -17,28 +17,27 @@
     </div>
   </li>
 </ul>
-</div>
+</div> -->
 
-<!-- 
-<table class="table-auto">
-  <thead>
-    <tr>
-      <th>Clinic Name</th>
-      <th>Email</th>
-      <th>Phone</th>
-      <th>adres</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-     <td>{{$clinic->name}}</td>
-     <td>{{$clinic->email}}</td>
-     <td>{{$clinic->phone}}</td>
-     <td>{{$clinic->adress}}</td>
-    </tr>
-    
-  </tbody>
-</table> -->
+<table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th>Clinic Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Adress</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{{$clinic->name}}</td>
+            <td>{{$clinic->email}}</td>
+            <td>{{$clinic->phone}}</td>
+            <td>{{$clinic->adress}}</td>
+        </tr>
+    </tbody>
+</table>
 
 <?php endforeach ?>
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+  <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 </head>
 <body >
 
