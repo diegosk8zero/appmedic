1ade8b2062792b211d16bf99e6bde1885c4cfa99
Diego F. Ferreira <diegosk8zero@gmail.com>
2024-11-07 13:25:32 +0100

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,10 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <script src="https://cdn.tailwindcss.com"></script>
-  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script> -->
-  <link rel="stylesheet" href="{{asset('datatables.net-dt\css\dataTables.dataTables.min.css')}}" />
-  <script src="{{asset('datatables.net-dt\js\dataTables.dataTables.js')}}"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+ 
 </head>
 <body >
 
@@ -18,11 +17,4 @@
 
 
 </body>
-<script>
-import DataTable from 'datatables.net-dt';
- 
- let table = new DataTable('#myTable', {
-     // config options...
- });
-</script>
 </html>
