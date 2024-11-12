aa4d5915444f23bbe6d76dc577d180e0f8596419
Diego F. Ferreira <diegosk8zero@gmail.com>
2024-11-12 14:06:45 +0100

    tabela sickness
    models, controller, requests sickness

---
+++ app/Http/Controllers/SicknessController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sickness;
+use App\Http\Requests\StoreSicknessRequest;
+use App\Http\Requests\UpdateSicknessRequest;
+
+class SicknessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSicknessRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sickness $sickness)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sickness $sickness)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSicknessRequest $request, Sickness $sickness)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sickness $sickness)
+    {
+        //
+    }
+}
